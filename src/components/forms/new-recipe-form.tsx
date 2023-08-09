79084006ebd4b4b4e44c2807c28d4e4ed3f46db9
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-08 19:35:24 -0600

    feat: 🎸 add new recipe form

---
+++ src/components/forms/new-recipe-form.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+})
+
+export function AddNewRecipe() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  })
+  form.watch((data) => console.log(data))
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <form
+        className="grid w-full max-w-2xl gap-5"
+        onSubmit={(...args) => void form.handleSubmit(onSubmit)(...args)}
+      >
+        <FormItem>
+          <FormLabel>Name</FormLabel>
+          <FormControl>
+            <Input
+              aria-invalid={!!form.formState.errors.username}
+              placeholder="Type product name here."
+              {...form.register("username")}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+
+
+
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  )
+}
