9eeb79b9da49a56d91db98604f0521ad7a4535ec
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-08 19:37:05 -0600

    feat: 🎸 new recipe page

---
+++ src/app/(dashboard)/dashboard/recipes/new/page.tsx
@@ -0,0 +1,10 @@
+import { AddNewRecipe } from '@/components/forms/new-recipe-form'
+
+export default function NewRecipePage() {
+  return (
+    <div className='ml-3'>
+      <h1>New Recipe</h1>
+      <AddNewRecipe />
+    </div>
+  )
+}
