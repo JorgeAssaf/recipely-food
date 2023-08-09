6ab66f64bdc6c147cdb7bb0b2447816252eb1238
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-08 19:36:01 -0600

    feat: 🎸 add clerk components

---
+++ src/app/(auth)/auth/signout/page.tsx
@@ -0,0 +1,19 @@
+import { LogOutButtons } from "@/components/auth/logout-button"
+import { Shell } from "@/components/shell"
+
+// Running out of edge function execution units on vercel free plan
+// export const runtime = "edge"
+
+export default function SignOutPage() {
+  return (
+    <Shell className="max-w-xs">
+      {/* <Header
+        title="Sign out"
+        description="Are you sure you want to sign out?"
+        size="sm"
+        className="text-center"
+      /> */}
+      <LogOutButtons />
+    </Shell>
+  )
+}
