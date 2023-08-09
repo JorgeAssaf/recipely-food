b5e7de7d2044dba6f4540ef1a25be0112416688d
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-08 19:34:09 -0600

    feat: 🎸 add user

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -1,21 +1,19 @@
-// import { currentUser } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs"
 
 import SiteFooter from "@/components/layout/site-footer"
 import SiteHeader from "@/components/layout/site-header"
 
-// import { SiteFooter } from "@/components/layouts/site-footer"
-// import { SiteHeader } from "@/components/layouts/site-header"
 
 interface LobbyLayoutProps {
   children: React.ReactNode
 }
 
 export default async function LobbyLayout({ children }: LobbyLayoutProps) {
-  // const user = await currentUser()
+  const user = await currentUser()
 
   return (
     <div className='relative flex min-h-screen flex-col'>
-      <SiteHeader />
+      <SiteHeader user={user} />
       <main className='flex-1'>{children}</main>
       <SiteFooter />
     </div>
