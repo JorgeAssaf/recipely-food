483f5f1b0dcecba9a48025058cc5a488d7f46cd7
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-14 20:34:34 -0600

    fix: 🐛 mark book only if user exist

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -48,7 +48,7 @@ const SiteHeader: FC<SiteHeaderProps> = ({ user }) => {
         <MobileNav mainNavItemItems={siteConfig.MainNavItem} />
         <div className='flex flex-1 items-center justify-end space-x-4'>
           <nav className='flex items-center space-x-2'>
-            <BookmarkIcon />
+            {user ? <BookmarkIcon /> : null}
             {user ? (
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
