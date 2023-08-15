ee24a9e11536aa6a76aa4ee3a7caf7e8cc4b5a80
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-14 19:00:44 -0600

    feat: 🎸 add search

--- src/components/combobox.tsx
+++ src/components/combobox.tsx
@@ -1,27 +1,69 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { SearchIcon } from 'lucide-react'
 
 import { isMacOs } from '@/lib/utils'
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command'
 
 import { Button } from './ui/button'
 
 const Combobox = () => {
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+    document.addEventListener('keydown', down)
+    return () => document.removeEventListener('keydown', down)
+  }, [])
   return (
-    <Button
-      variant='outline'
-      className='relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2'
-    >
-      <SearchIcon className='h-4 w-4 xl:mr-2' aria-hidden='true' />
-      <span className='hidden xl:inline-flex'>Search Recipes...</span>
-      <span className='sr-only'>Search Recipes</span>
-      <kbd className='pointer-events-none absolute right-1.5 top-2 hidden h-6 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 xl:flex'>
-        <abbr title={isMacOs() ? 'Command' : 'Control'}>
-          {isMacOs() ? '⌘' : 'Ctrl+'}
-        </abbr>
-        K
-      </kbd>
-    </Button>
+    <>
+      <Button
+        onClick={() => setOpen(true)}
+        variant='outline'
+        className='relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2'
+      >
+        <SearchIcon className='h-4 w-4 xl:mr-2' aria-hidden='true' />
+        <span className='hidden xl:inline-flex'>Search Recipes...</span>
+        <span className='sr-only'>Search Recipes</span>
+        <kbd className='pointer-events-none absolute right-1.5 top-2 hidden h-6 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 xl:flex'>
+          <abbr title={isMacOs() ? 'Command' : 'Control'}>
+            {isMacOs() ? '⌘' : 'Ctrl+'}
+          </abbr>
+          K
+        </kbd>
+      </Button>
+      <CommandDialog position='top' open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder='Search products...' />
+        <CommandList>
+          <CommandEmpty>No products found.</CommandEmpty>
+          <CommandGroup heading='Suggestions'>
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading='Settings'>
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   )
 }
 export default Combobox
