32a2b2e5fdb4e4fef3372d80751254f485e41518
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-17 14:00:28 -0600

    feat: 🎸 ad utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,9 +15,41 @@ export function slugify(text: string) {
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, '')
 }
-
-export function isMacOs() {
-  if (typeof window === 'undefined') return false;
-
-  return window.navigator.userAgent.includes('Mac');
+export function toTitleCase(str: string) {
+  return str.replace(
+    /\w\S*/g,
+    (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase(),
+  )
 }
+export function isMacOs() {
+  if (typeof window === 'undefined') return false
+
+  return window.navigator.userAgent.includes('Mac')
+}
+export type As<
+  DefaultTag extends React.ElementType,
+  T1 extends React.ElementType,
+  T2 extends React.ElementType = T1,
+  T3 extends React.ElementType = T1,
+  T4 extends React.ElementType = T1,
+  T5 extends React.ElementType = T1,
+> =
+  | (React.ComponentPropsWithRef<DefaultTag> & {
+    as?: DefaultTag
+  })
+  | (React.ComponentPropsWithRef<T1> & {
+    as: T1
+  })
+  | (React.ComponentPropsWithRef<T2> & {
+    as: T2
+  })
+  | (React.ComponentPropsWithRef<T3> & {
+    as: T3
+  })
+  | (React.ComponentPropsWithRef<T4> & {
+    as: T4
+  })
+  | (React.ComponentPropsWithRef<T5> & {
+    as: T5
+  })
+
