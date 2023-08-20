a8ce54adb090205d0d711e757608542023daf2d2
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-20 13:09:10 -0600

    feat: 🎸 add card

--- src/app/(dashboard)/dashboard/recipes/new/page.tsx
+++ src/app/(dashboard)/dashboard/recipes/new/page.tsx
@@ -1,10 +1,25 @@
 import { AddNewRecipe } from '@/components/forms/new-recipe-form'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 
 export default function NewRecipePage() {
   return (
-    <div className='ml-3'>
-      <h1>New Recipe</h1>
-      <AddNewRecipe />
-    </div>
+
+    <Card>
+      <CardHeader className='space-y-1'>
+        <CardTitle className='text-2xl'>Add New Recipe</CardTitle>
+        <CardDescription> Add a new recipe to your collection.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <AddNewRecipe />
+      </CardContent>
+    </Card>
+
+
   )
 }
