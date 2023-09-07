a34ff7a1abb65a557de98aa0edc7fbf87866e592
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-06 21:17:36 -0600

    feat: 🎸 add recipes icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,9 +1,28 @@
-import { Book, CreditCard, User2, type LucideIcon } from 'lucide-react'
+import {
+  Apple,
+  Book,
+  CakeSlice,
+  Coffee,
+  CreditCard,
+  EggFried,
+  Grape,
+  Lollipop,
+  User2,
+  Wine,
+  type LucideIcon,
+} from 'lucide-react'
 
 export type Icon = LucideIcon
 
-export const icons = {
+export const Icons = {
   creditCard: CreditCard,
   book: Book,
   user: User2,
+  breakfast: EggFried,
+  lunch: Apple,
+  dinner: Coffee,
+  dessert: CakeSlice,
+  snack: Grape,
+  drinks: Wine,
+  appetizer: Lollipop,
 }
