c3e8f930c1baa323a008e2d83ae77a312bfea1a6
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-08 11:37:58 -0600

    feat: 🎸 add getRecipesAction

--- src/app/_actions/recipes.ts
+++ src/app/_actions/recipes.ts
@@ -2,11 +2,11 @@
 
 import { revalidatePath } from 'next/cache'
 import { db } from '@/db'
-import { ingredients, recipes } from '@/db/schema'
-import { like } from 'drizzle-orm'
+import { ingredients, Recipes, recipes } from '@/db/schema'
+import { and, asc, desc, eq, inArray, like, sql } from 'drizzle-orm'
 import { z } from 'zod'
 
-import { recipesSchema } from '@/lib/validations/recipes'
+import { getRecipesSchema, recipesSchema } from '@/lib/validations/recipes'
 
 export async function filterProductsAction(query: string) {
   if (query.length === 0) return null
@@ -25,6 +25,75 @@ export async function filterProductsAction(query: string) {
     recipes: filteredRecipes.filter((recipe) => recipe.category === category),
   }))
   return data
+}
+export async function getRecipesAction(
+  input: z.infer<typeof getRecipesSchema>,
+) {
+  const [column, order] =
+    (input.sort?.split('.') as [
+      keyof Recipes | undefined,
+      'asc' | 'desc' | undefined,
+    ]) ?? []
+  const difficulty =
+    (input.difficulty?.split('.') as Recipes['difficulty'][]) ?? []
+  const prepTime = input.prepTime
+  const categories =
+    (input.categories?.split('.') as Recipes['category'][]) ?? []
+  const limit = input.limit ?? 2
+  const offset = input.offset ?? 0
+
+  const { items, count } = await db.transaction(async (tx) => {
+    const items = await tx
+      .select()
+      .from(recipes)
+      .limit(limit)
+      .offset(offset)
+      .where(
+        and(
+          categories.length ? inArray(recipes.category, categories) : undefined,
+          difficulty.length
+            ? inArray(recipes.difficulty, difficulty)
+            : undefined,
+          prepTime ? eq(recipes.prepTime, prepTime) : undefined,
+
+        ),
+      )
+      .groupBy(recipes.id)
+      .orderBy(
+        column && column in recipes
+          ? order === 'asc'
+            ? asc(recipes[column])
+            : desc(recipes[column])
+          : desc(recipes.createdAt),
+      )
+    const count = await tx
+      .select({
+        count: sql<number>`count(*)`,
+      })
+      .from(recipes)
+      .where(
+        and(
+          categories.length ? inArray(recipes.category, categories) : undefined,
+          difficulty.length
+            ? inArray(recipes.difficulty, difficulty)
+            : undefined,
+          prepTime ? eq(recipes.prepTime, prepTime) : undefined,
+        ),
+      )
+      .groupBy(recipes.id)
+      .execute()
+      .then((res) => res[0]?.count ?? 0)
+
+    return {
+      items,
+      count,
+    }
+  })
+
+  return {
+    items,
+    count,
+  }
 }
 
 export async function AddRecipeAction(values: z.infer<typeof recipesSchema>) {
