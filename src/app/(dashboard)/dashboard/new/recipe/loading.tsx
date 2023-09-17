49cbb058069973e908fa2eeb00878145b18a3fd8
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-17 10:58:43 -0600

    feat: 🎸 add loading page

--- src/app/(dashboard)/dashboard/new/recipe/loading.tsx
+++ src/app/(dashboard)/dashboard/new/recipe/loading.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
 
 export default function NewProductLoading() {
@@ -8,8 +8,35 @@ export default function NewProductLoading() {
         <Skeleton className='h-6 w-1/4' />
         <Skeleton className='h-4 w-2/4' />
       </CardHeader>
-      <CardContent>
-        <div className='grid w-full max-w-xl gap-4'>
+      <CardContent className='h-auto'>
+        <div className='grid w-full  max-w-xl gap-10'>
+          <div className='space-y-2.5'>
+            <Skeleton className='h-4 w-32' />
+            <Skeleton className='h-6' />
+          </div>
+          <div className='space-y-2.5'>
+            <Skeleton className='h-4 w-32' />
+            <Skeleton className='h-20' />
+          </div>
+          <div className='space-y-2.5'>
+            <Skeleton className='h-4 w-32' />
+            <Skeleton className='h-6' />
+          </div>
+          <div className='space-y-2.5'>
+            <Skeleton className='h-4 w-32' />
+
+            <div className='grid grid-cols-4 gap-4'>
+              <Skeleton className='h-6' />
+              <Skeleton className='h-6' />
+              <Skeleton className='h-6' />
+              <Skeleton className='h-6' />
+            </div>
+          </div>
+          <div className='space-y-2.5'>
+            <Skeleton className='h-4 w-32' />
+            <Skeleton className='h-2 w-24' />
+            <Skeleton className='h-6' />
+          </div>
           <div className='space-y-2.5'>
             <Skeleton className='h-4 w-32' />
             <Skeleton className='h-6' />
@@ -20,9 +47,6 @@ export default function NewProductLoading() {
           </div>
         </div>
       </CardContent>
-      <CardFooter>
-        <Skeleton className='h-10 w-32' />
-      </CardFooter>
     </Card>
   )
 }
