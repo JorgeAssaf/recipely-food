e2fe176372d4cedbfda7da57db238c7948b5b102
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-17 10:53:11 -0600

    refactor: 💡 update page and refactor loading page

---
+++ src/app/(dashboard)/dashboard/new/recipe/page.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+import { currentUser } from '@clerk/nextjs/server'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { AddNewRecipe } from '@/components/forms/new-recipe-form'
+
+export const metadata: Metadata = {
+  title: 'New Recipe',
+  description: 'Add a new recipe to your collection.',
+}
+
+export default async function NewRecipePage() {
+  const user = await currentUser()
+  if (!user) {
+    return redirect('/sign-in')
+  }
+  return (
+    <Card>
+      <CardHeader className='space-y-1'>
+        <CardTitle className='text-2xl'>Add New Recipe</CardTitle>
+        <CardDescription> Add a new recipe to your collection.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <AddNewRecipe userId={user.id} userName={user.firstName + ' ' + user.lastName} />
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/app/(dashboard)/dashboard/recipes/loading.tsx
@@ -0,0 +1,6 @@
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function DashboardLoading() {
+  return 'Loading...'
+}
