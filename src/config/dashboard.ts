d34343333a70d7a8dc270d1c981fe10ee663884a
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-17 20:04:13 -0600

    refactor: 💡 dashboard items

--- src/config/dashboard.ts
+++ src/config/dashboard.ts
@@ -7,28 +7,34 @@ export interface DashboardConfig {
 export const dashboardConfig: DashboardConfig = {
   sidebarNav: [
     {
-      title: 'Account',
-      href: '/dashboard/account',
-      icon: 'user',
-      items: [],
-    },
-    {
-      title: 'Recipes',
-      href: '/dashboard/recipes',
-      icon: 'book',
-      items: [],
-    },
-    {
-      title: 'Billing',
-      href: '/dashboard/billing',
-      icon: 'creditCard',
-      items: [],
-    },
-    {
-      title: 'Purchases',
-      href: '/dashboard/purchases',
-      icon: 'creditCard',
-      items: [],
+      title: 'Dashboard',
+      href: '/dashboard',
+      items: [
+        {
+          title: 'Account',
+          href: '/dashboard/account',
+          icon: 'user',
+          items: [],
+        },
+        {
+          title: 'Recipes',
+          href: '/dashboard/recipes',
+          icon: 'book',
+          items: [],
+        },
+        {
+          title: 'Billing',
+          href: '/dashboard/billing',
+          icon: 'creditCard',
+          items: [],
+        },
+        {
+          title: 'Purchases',
+          href: '/dashboard/purchases',
+          icon: 'creditCard',
+          items: [],
+        },
+      ],
     },
   ],
 }
