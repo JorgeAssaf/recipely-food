d26876fe210ededc737a9cdd9ccb4c1367edc5be
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-17 21:03:18 -0600

    feat: 🎸 add your recipes page

---
+++ src/app/(dashboard)/dashboard/recipes/[recipeId]/recipe/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Update Recipe',
+  description: 'Update a recipe in your collection.',
+}
+
+export default async function RecipePage() {
+  return (
+    <div>
+      <h1>Update Recipe</h1>
+    </div>
+
+  )
+}
