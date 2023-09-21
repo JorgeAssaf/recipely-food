1b0639a1695fae7c89cbe09e04489c04a54df376
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-20 21:48:21 -0600

    feat: 🎸 password input

---
+++ src/components/password-input.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { forwardRef, useState } from 'react'
+import { EyeIcon, EyeOffIcon } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+
+import { Button } from './ui/button'
+import { Input, type InputProps } from './ui/input'
+
+const PasswordInput = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(false)
+    return (
+      <div className='relative'>
+        <Input
+          type={showPassword ? 'text' : 'password'}
+          ref={ref}
+          className={cn('pr-10', className)}
+          {...props}
+        />
+        <Button
+          type='button'
+          variant='ghost'
+          size='icon'
+          className='absolute right-0 top-0 h-full px-3 py-1 hover:bg-transparent'
+          onClick={() => setShowPassword((prev) => !prev)}
+        >
+          {showPassword ? (
+            <EyeIcon className='h-5 w-5' />
+          ) : (
+            <EyeOffIcon className='h-5 w-5' />
+          )}
+          <span className='sr-only'>
+            {showPassword ? 'Hide password' : 'Show password'}
+          </span>
+        </Button>
+      </div>
+    )
+  },
+)
+
+export default PasswordInput
