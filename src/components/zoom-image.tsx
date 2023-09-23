e7094b7cb327106419bb1967c1f057a55bacccf5
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-22 22:25:22 -0600

    feat: 🎸 add zoom componet

---
+++ src/components/zoom-image.tsx
@@ -0,0 +1,17 @@
+import type { HTMLAttributes } from 'react'
+import MediumZoom from 'react-medium-image-zoom'
+
+import 'react-medium-image-zoom/dist/styles.css'
+
+export function Zoom({ children }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <MediumZoom
+      classDialog='zoom-image'
+      zoomMargin={30}
+      a11yNameButtonZoom='Zoom'
+      a11yNameButtonUnzoom='Unzoom'
+    >
+      {children}
+    </MediumZoom>
+  )
+}
