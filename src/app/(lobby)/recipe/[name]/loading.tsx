0e80e1fdd85f08172e46345d7929f2ca2852fb35
Jorge Assaf <jorgeassaf160@gmail.com>
2023-10-05 11:53:40 -0600

    refactor: 💡 recipe loading

--- src/app/(lobby)/recipe/[name]/loading.tsx
+++ src/app/(lobby)/recipe/[name]/loading.tsx
@@ -1,47 +1,42 @@
 import { ImageIcon } from 'lucide-react'
 
-import { AspectRatio } from '@/components/ui/aspect-ratio'
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Shell } from '@/components/shell'
 
 export default function RecipeLoading() {
   return (
     <Shell>
-      <div className='space-y-3'>
-        <Skeleton className='h-10 w-28' />
-        <Skeleton className='h-4 w-60' />
-        <div className='flex items-center gap-2'>
-          <Skeleton className='h-9 w-14' />
-          <Skeleton className='h-9 w-14' />
+      <div className='mb-5 flex items-center gap-5'>
+        <div>
+          <Skeleton className='h-8 w-20' />
+        </div>
+        <div>
+          <Skeleton className='h-8 w-20' />
+        </div>
+        <div>
+          <Skeleton className='h-8 w-20' />
+        </div>
+        <div>
+          <Skeleton className='h-8 w-20' />
         </div>
       </div>
 
-      <div className='mb-5 flex flex-col'>
-        <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {new Array(6).fill(0).map((_, i) => (
-            <Card key={i} className='rounded-sm'>
-              <CardHeader className='border-b p-0'>
-                <AspectRatio ratio={16 / 9}>
-                  <div className='flex h-full items-center justify-center bg-secondary'>
-                    <ImageIcon
-                      className='h-9 w-9 text-muted-foreground'
-                      aria-hidden='true'
-                    />
-                  </div>
-                </AspectRatio>
-              </CardHeader>
-              <CardContent className='grid gap-2.5 p-4'>
-                <Skeleton className='h-4 w-1/2' />
-                <Skeleton className='h-4 w-1/4' />
-              </CardContent>
-              <CardFooter className='p-4'>
-                <Skeleton className='h-8 w-full rounded-sm' />
-              </CardFooter>
-            </Card>
-          ))}
+      <section className='grid grid-cols-1 gap-7 md:grid-cols-2'>
+        <div
+          aria-label='Product Placeholder'
+          role='img'
+          aria-roledescription='placeholder'
+          className='flex aspect-square h-full w-full flex-1 items-center justify-center '
+        >
+          <div className='flex flex-col items-center justify-center gap-4'>
+            <ImageIcon
+              className='h-9 w-9 text-muted-foreground'
+              aria-hidden='true'
+            />
+          </div>
         </div>
-      </div>
+        <Skeleton className='h-auto rounded-2xl rounded-bl-2xl ' />
+      </section>
     </Shell>
   )
 }
