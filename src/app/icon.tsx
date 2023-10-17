b17de853d4952e1db2b415aa3137aaf8777365fd
Jorge Assaf <jorgeassaf160@gmail.com>
2023-10-17 10:28:34 -0600

    update icon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -16,20 +16,10 @@ export default function Icon() {
     (
       // ImageResponse JSX element
       <div
-        style={{
-          fontSize: 24,
-          background: 'black',
-          borderRadius: '100%',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          color: 'white',
-        }}
+        tw='flex h-full w-full items-center justify-center rounded-full bg-black text-white'
+
       >
         <svg
-          xmlns='http://www.w3.org/2000/svg'
           width='24'
           height='24'
           viewBox='0 0 24 24'
@@ -38,7 +28,7 @@ export default function Icon() {
           stroke-width='2'
           stroke-linecap='round'
           stroke-linejoin='round'
-          className=' h-5 w-5 md:h-[1.55rem] md:w-[1.55rem]'
+          style={{ height: '1.55rem', width: '1.55rem' }}
         >
           <path d='M6 13.87A4 4 0 0 1 7.41 6a5.11 5.11 0 0 1 1.05-1.54 5 5 0 0 1 7.08 0A5.11 5.11 0 0 1 16.59 6 4 4 0 0 1 18 13.87V21H6Z'></path>
           <line x1='6' x2='18' y1='17' y2='17'></line>
