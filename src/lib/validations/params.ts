51e060bb12a13fcf164a6a85a9fe57fbb45989c8
Jorge Assaf <jorgeassaf160@gmail.com>
2023-10-23 12:59:34 -0600

    createdAt.desc

--- src/lib/validations/params.ts
+++ src/lib/validations/params.ts
@@ -6,7 +6,7 @@ export const searchParamsSchema = z.object({
 })
 
 export const recipesParamsSchema = searchParamsSchema.extend({
-  sort: z.string().optional().default('createdAt.asc'),
+  sort: z.string().optional().default('createdAt.desc'),
   categories: z.string().optional(),
   prepTime: z.string().optional(),
   difficulty: z.string().optional(),
