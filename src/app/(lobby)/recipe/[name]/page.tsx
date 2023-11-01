ebf601d4ccf7c617540d26d296a1702fd15e16a3
Jorge Assaf <jorgeassaf160@gmail.com>
2023-10-31 22:03:46 -0600

    feat: 🎸 add keywords

--- src/app/(lobby)/recipe/[name]/page.tsx
+++ src/app/(lobby)/recipe/[name]/page.tsx
@@ -24,6 +24,7 @@ export async function generateMetadata({
     columns: {
       name: true,
       description: true,
+      category: true,
     },
     where: (recipes, { eq }) => eq(recipes.name, name),
   })
@@ -31,6 +32,7 @@ export async function generateMetadata({
   return {
     title: recipe?.name,
     description: recipe?.description,
+    keywords: [`${recipe?.name}`, `${recipe?.category}`],
   }
 }
 
