2b28f64a2647e31282358ef4f7e4f7de54c7a57c
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-12 14:29:02 -0600

    :recycle: Refactor Icon component to use ImageResponse

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,45 +1,42 @@
 import { ImageResponse } from 'next/og'
 
-
+// Route segment config
 export const runtime = 'edge'
 
 // Image metadata
 export const size = {
-  width: 32,
-  height: 32,
+    width: 32,
+    height: 32,
 }
 export const contentType = 'image/png'
 
-
+// Image generation
 export default function Icon() {
-  return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <div
-        tw='flex h-full w-full items-center justify-center rounded-full bg-black text-white'
-
-      >
-        <svg
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          stroke-width='2'
-          stroke-linecap='round'
-          stroke-linejoin='round'
-          style={{ height: '1.55rem', width: '1.55rem' }}
-        >
-          <path d='M6 13.87A4 4 0 0 1 7.41 6a5.11 5.11 0 0 1 1.05-1.54 5 5 0 0 1 7.08 0A5.11 5.11 0 0 1 16.59 6 4 4 0 0 1 18 13.87V21H6Z'></path>
-          <line x1='6' x2='18' y1='17' y2='17'></line>
-        </svg>
-      </div>
-    ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported icons size metadata
-      // config to also set the ImageResponse's width and height.
-      ...size,
-    },
-  )
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div tw='flex h-full w-full items-center justify-center rounded-full bg-black text-white'>
+                <svg
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    stroke-width='2'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    style={{ height: '1.55rem', width: '1.55rem' }}
+                >
+                    <path d='M6 13.87A4 4 0 0 1 7.41 6a5.11 5.11 0 0 1 1.05-1.54 5 5 0 0 1 7.08 0A5.11 5.11 0 0 1 16.59 6 4 4 0 0 1 18 13.87V21H6Z'></path>
+                    <line x1='6' x2='18' y1='17' y2='17'></line>
+                </svg>
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported icons size metadata
+            // config to also set the ImageResponse's width and height.
+            ...size,
+        },
+    )
 }
