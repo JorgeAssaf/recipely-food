3fd6e59ff8be8df6006f8d2afd8439f603679706
Jorge Assaf <jorgeassaf160@gmail.com>
2024-04-18 18:33:49 -0600

    :memo: Update site-footer.tsx - Fix link to personal website

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -71,7 +71,7 @@ const SiteFooter = () => {
           <div className='flex-1 text-left text-sm leading-loose text-muted-foreground'>
             Built by{' '}
             <Link
-              href='https://twitter.com/jorgeassaf'
+              href='https://jorgeassaf.vercel.app/'
               target='_blank'
               rel='noreferrer'
               className='font-semibold transition-colors hover:text-foreground'
