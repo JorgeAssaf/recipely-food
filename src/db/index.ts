069558781d4c6caaa16e1250a2b05a8eb33e2523
Jorge Assaf <jorgeassaf160@gmail.com>
2024-09-30 19:31:23 -0600

    Refactor database singleton connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,21 @@
-import { drizzle } from "drizzle-orm/mysql2";
-import { createPool } from "mysql2/promise";
-import * as schema from './schema';
+import { drizzle } from 'drizzle-orm/mysql2'
+import { createPool } from 'mysql2/promise'
+
+import * as schema from './schema'
+
+declare const globalThis: {
+  _db: ReturnType<typeof drizzle>;
+} & typeof global;
+
 
 const connection = createPool({
   uri: process.env.DATABASE_URL!,
-});
+})
 
+const db = globalThis._db ?? drizzle(connection, { schema, mode: 'default' })
 
-export const db = drizzle(connection, { schema, mode: 'default' });
+if (process.env.NODE_ENV !== 'production') {
+  globalThis._db = db
+}
+
+export { db }
