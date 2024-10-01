d33dc6ccce809934250fe18c629baa606146f3b7
Jorge Assaf <jorgeassaf160@gmail.com>
2024-10-01 15:51:05 -0600

    ♻️ Refactor config matcher in middleware.ts to skip Next.js internals and static files, unless found in search params, and always run for API routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,7 +30,10 @@ export default clerkMiddleware(async (auth, req) => {
 })
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
