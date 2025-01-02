32b2e01bca5314ce5b3f8043537ef7063ee045da
Jorge Assaf <jorgeassaf160@gmail.com>
2025-01-01 20:26:37 -0600

    ✨ Add generateReactHelpers to uploadthing.ts for enhanced upload functionality

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,9 +1,12 @@
 import {
+  generateReactHelpers,
   generateUploadButton,
   generateUploadDropzone,
-} from "@uploadthing/react";
+} from '@uploadthing/react'
 
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import type { OurFileRouter } from '@/app/api/uploadthing/core'
 
-export const UploadButton = generateUploadButton<OurFileRouter>();
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const UploadButton = generateUploadButton<OurFileRouter>()
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>()
