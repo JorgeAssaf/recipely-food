ef8c62a1be4c44c927d9bf82b2f2596fef2e150c
Jorge Assaf <jorgeassaf160@gmail.com>
2025-04-12 22:28:56 -0600

    ♻️ Migrate database from MySQL to PostgreSQL and update schema definitions

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,13 @@
-import { drizzle } from 'drizzle-orm/mysql2'
-import { createPool } from 'mysql2/promise'
 
 import * as schema from './schema'
 
-declare const globalThis: {
-  _db: ReturnType<typeof drizzle<typeof schema>>
-} & typeof global
-
-const connection = createPool({
-  uri: process.env.DATABASE_URL!,
-})
-
-const db = globalThis._db ?? drizzle(connection, { schema, mode: 'default' })
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis._db = db
-}
-
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    ca: process.env.DATABASE_CA,
+  }
+});
+const db = drizzle({ client: pool, schema });
 export { db }
