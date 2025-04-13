f9a848b0c597d5f25ecbeecb07e85e3f157dd7ee
Jorge Assaf <jorgeassaf160@gmail.com>
2025-04-12 22:27:57 -0600

    ♻️ Refactor drizzle.config.ts to use defineConfig and update database dialect to postgresql

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,22 @@
 import * as dotenv from 'dotenv'
-import type { Config } from 'drizzle-kit'
+import { defineConfig, type Config } from 'drizzle-kit'
 
 dotenv.config()
 
-export default {
-  schema: './src/db/schema.ts',
-  driver: 'mysql2',
+export default defineConfig({
   out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
   dbCredentials: {
-    uri: process.env.DATABASE_URL!,
+    // using url doesn't work because it seems to override the ssl config
+    url: process.env.DATABASE_URL!,
+    host: process.env.DATABASE_HOST!,
+    port: parseInt(process.env.DATABASE_PORT!),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database: process.env.DATABASE_NAME!,
+    ssl: {
+      ca: process.env.DATABASE_CA!,
+    },
   },
-} satisfies Config
+}) satisfies Config
