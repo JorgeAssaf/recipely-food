0d7ecfc3787d582652e9dda56881e377158acc96
Jorge Assaf <jorgeassaf160@gmail.com>
2025-04-12 22:33:37 -0600

    🎨 Format code

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,13 @@
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 
 import * as schema from './schema'
 
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     ca: process.env.DATABASE_CA,
-  }
-});
-const db = drizzle({ client: pool, schema });
+  },
+})
+const db = drizzle({ client: pool, schema })
 export { db }
