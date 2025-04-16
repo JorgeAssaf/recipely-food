948f89cdc86d4d94dc503fe7c2d7a457aaa00acc
Jorge Assaf <jorgeassaf160@gmail.com>
2025-04-16 14:08:08 -0600

    🐛 Fix formatting and improve readability in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -54,9 +54,10 @@ export function toTitleCase(str: string) {
     (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase(),
   )
 }
-export function isMacOs() {
-  if (typeof window === 'undefined') return false
-
+export function isMacOs(): boolean {
+  if (typeof window === 'undefined') {
+    return false
+  }
   return window.navigator.userAgent.includes('Mac')
 }
 export function catchClerkError(err: unknown) {
@@ -88,20 +89,21 @@ export type As<
   T5 extends React.ElementType = T1,
 > =
   | (React.ComponentPropsWithRef<DefaultTag> & {
-      as?: DefaultTag
-    })
+    as?: DefaultTag
+  })
   | (React.ComponentPropsWithRef<T1> & {
-      as: T1
-    })
+    as: T1
+  })
   | (React.ComponentPropsWithRef<T2> & {
-      as: T2
-    })
+    as: T2
+  })
   | (React.ComponentPropsWithRef<T3> & {
-      as: T3
-    })
+    as: T3
+  })
   | (React.ComponentPropsWithRef<T4> & {
-      as: T4
-    })
+    as: T4
+  })
   | (React.ComponentPropsWithRef<T5> & {
-      as: T5
-    })
+    as: T5
+  })
+
