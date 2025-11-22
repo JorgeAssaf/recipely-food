5082897d1132566305eec22e5f89bb0696dc8c73
Jorge Assaf <jorgeassaf160@gmail.com>
2025-11-21 22:45:12 -0600

    🔇 Remove debug log statement from RecipesPage for cleaner code

--- src/app/(lobby)/recipes/page.tsx
+++ src/app/(lobby)/recipes/page.tsx
@@ -33,7 +33,6 @@ export default async function RecipesPage({ searchParams }: RecipePageProps) {
   const fallbackPage =
     isNaN(pageAsNumber) || pageAsNumber < 1 ? 1 : pageAsNumber
   const perPageAsNumber = Number(per_page)
-  console.log(sort)
   const limit = isNaN(perPageAsNumber) ? 8 : perPageAsNumber
   const offset = fallbackPage > 0 ? (fallbackPage - 1) * limit : 0
   const recipesTransaction = await getRecipesAction({
